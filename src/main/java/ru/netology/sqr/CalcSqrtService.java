ebcfdefa1cd5b3a4ba1448b8bd3e27e7c1ae4c06
NataBarsu <khvorostukhina.n@yandex.ru>
2022-07-18 18:19:19 +0300

    CalcSqrtService changed

--- src/main/java/ru/netology/sqr/CalcSqrtService.java
+++ src/main/java/ru/netology/sqr/CalcSqrtService.java
@@ -1,14 +1,14 @@
 package ru.netology.sqr;
 
 public class CalcSqrtService {
-    public int calcSqrt(int x, int y) {
-        int z = 0;
+    public int calcSqrt(int minvalue, int maxvalue) {
+        int quantityOfSqrt = 0;
         for (int i = 10; i <= 99; i++) {
-            if (i * i >= x && i * i <= y) {
-                z++;
+            if (i * i >= minvalue && i * i <= maxvalue) {
+                quantityOfSqrt++;
 
             }
         }
-        return z;
+        return quantityOfSqrt;
     }
 }
